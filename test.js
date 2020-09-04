5d8575dcb10e3d6b331f3561f227bca4b6830c79
Jacob Niedzwiecki <jake@jacob-n.com>
2020-09-03 23:12:09 -0600

    Tested on safari & chrome, tidied up & commented

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-const { Builder, By, Key, until } = require('selenium-webdriver')
+const { Builder, By, Key, until } = require('selenium-webdriver') // TODO figure out what jest-environment-webdriver is for
 
 const url = 'http://localhost:5000'
 
 describe('mud run', () => {
   var driver
   beforeAll( async () => {
-    driver = await new Builder().forBrowser('chrome').build()
+    driver = await new Builder().forBrowser('safari').build()
 
     await driver.get(url)
     await driver.wait(until.titleIs('Subscrape'), 1000)
@@ -15,9 +15,12 @@ describe('mud run', () => {
     await driver.quit()
   }, 15000)
 
-  test('that the video element exists', async () => {
-    let videoElement = await driver.findElement(By.id('test_video'))
-    console.log(videoElement)
-    expect(videoElement).toBeDefined()
+  test('that the first caption displays', async () => {
+    let mediaElement = await driver.findElement(By.id('test_media'))
+    expect(mediaElement).toBeDefined()
+
+    let caption = await driver.findElement(By.id('caption_display')).getText()
+    // expect(caption).toEqual('Test')
+    expect(caption).toEqual('Japan is an island country in East Asia and is roughly')
   })
 })
